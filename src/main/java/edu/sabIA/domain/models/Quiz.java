cdf3349f19c279921d437cc6757bb9eaf33d9920
Yohans Nascimento <51249906+YohansN@users.noreply.github.com>
2025-11-20 11:57:19 -0300

    entidade criada (#1)
    Co-authored-by: Pedro Paulo Durand <pedropaulodurand@gmail.com>

---
+++ src/main/java/edu/sabIA/domain/models/Quiz.java
@@ -0,0 +1,55 @@
+package edu.sabIA.domain.models;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "quizzes")
+public class Quiz {
+    @Id
+    private UUID id;
+    private String theme;
+    private String[] topics;
+    private int numberOfQuestions;
+    private String quizJson;
+    private int currentQuestion;
+    private int score;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UUID userId;
+    private LocalDateTime createdAt;
+    private boolean isFinished;
+
+    public Quiz(String theme, int numberOfQuestions, String quizJson, int score, UUID userId) {
+        this.id = UUID.randomUUID();
+        this.theme = theme;
+        this.numberOfQuestions = numberOfQuestions;
+        this.quizJson = quizJson;
+        this.currentQuestion = 0;
+        this.score = 0;
+        this.userId = userId;
+        this.createdAt = LocalDateTime.now();
+        this.isFinished = false;
+    }
+
+    public Quiz(String theme, int numberOfQuestions, String[] topics, String quizJson, int score, UUID userId) {
+        this.id = UUID.randomUUID();
+        this.theme = theme;
+        this.numberOfQuestions = numberOfQuestions;
+        this.quizJson = quizJson;
+        this.topics = topics;
+        this.currentQuestion = 0;
+        this.score = 0;
+        this.userId = userId;
+        this.createdAt = LocalDateTime.now();
+        this.isFinished = false;
+    }
+
+
+}

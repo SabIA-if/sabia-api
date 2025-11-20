b39f3d9e3ad38ab107136507f30db8644326d860
Pedro Paulo Durand <pedropaulodurand@gmail.com>
2025-11-20 10:17:20 -0300

    primeiro commit

---
+++ sabIA/src/main/java/edu/sabIA/SabIaApplication.java
@@ -0,0 +1,13 @@
+package edu.sabIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SabIaApplication.class, args);
+	}
+
+}
---
+++ sabIA/src/test/java/edu/sabIA/SabIaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sabIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SabIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

1f9be2e18778cfe7eba1ac2e430931af8e80ed30
Pedro Paulo Durand <pedropaulodurand@gmail.com>
2025-11-20 12:02:27 -0300

    cirado a entidade

---
+++ src/main/java/edu/sabIA/domain/models/User.java
@@ -0,0 +1,30 @@
+package edu.sabIA.domain.models;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    private UUID id;
+    private String username;
+    private String name;
+    private String email;
+    private String passwordHash;
+    
+    public User(String username, String name, String email, String passwordHash) {
+        this.id = UUID.randomUUID();
+        this.username = username;
+        this.name = name;
+        this.email = email;
+        this.passwordHash = passwordHash;
+    }
+}
